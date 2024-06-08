37b56f3023c3af97bd32ce0e2f60fe6757c98c88
Tan Yong Rui <tanyongruifenway@gmail.com>
2024-06-08 13:40:23 +0800

    Added some jokes.

---
+++ src/cogs/jokes/joke_messages.py
@@ -0,0 +1,6 @@
+joke_list = [
+    "Life's hard, I ain't got enough? Well some people are hard, and they ain't got enough.",
+    "To succeed, first you got to succ, before you can get the ceed.",
+    "\"Sosi baka\" is a joke phrase combining Russian and Japanese words. It means \"suck it, idiot\" in a playful way. It's often used as a lighthea",
+    "Hard times create tough men, and tough times create hard men."
+]
